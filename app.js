bfe18378ccf5931c693be1f1d704c100e1621f04
Cem KARAKUŞ <karakuscemenes@gmail.com>
2022-08-09 21:58:19 +0300

    added change status and remove

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const addBook = document.querySelector(".btn");
 const form = document.querySelector(".form")
 const cardStatus = document.querySelectorAll(".bookStatus");
 const removeButton = document.querySelectorAll(".remove");
-const cardSection = document.getElementById("cards");
+const cardSection = document.querySelector("#cards");
 
 // Add Event Listener
 form.addEventListener("submit", (e) => {
@@ -48,17 +48,62 @@ function clearForm() {
 
 // Function for change status.
 
+
 //Render
 function renderBooks() {
-    cardSection.innerHTML ="";
+    cardSection.innerHTML = "";
     for (let i = 0; i < library.length; i++) {
-        const insertHTML = 
-        `<div class="bookCard">
-            <h1 class="bookTitle">Book Name: ${library[i].name}</h1>
-            <p class="bookAuthor">Author Name: ${library[i].author}</p>
-            <button class="bookStatus">Status: ${library[i].status}</button>
-            <button class="remove">Remove</button>`;
-        cardSection.insertAdjacentHTML("afterbegin", insertHTML);
+        const div = document.createElement('div');
+        div.classList.add("bookCard");
+        cardSection.appendChild(div);
+
+        const title = document.createElement('h1');
+        title.classList.add("bookTitle");
+        div.appendChild(title);
+
+        const para = document.createElement('p');
+        para.classList.add("bookAuthor");
+        div.appendChild(para);
+
+        const statusButton = document.createElement('button');
+        statusButton.classList.add("bookStatus");
+        statusButton.dataset.id = `${i}`;
+        div.appendChild(statusButton);
+
+        statusButton.addEventListener("click", changeStatus);
+        function changeStatus(){
+            let statusToChange = statusButton.dataset.id;
+            if (library[parseInt(statusToChange)].status === "Read") {
+                library[parseInt(statusToChange)].status = "Not Read";
+                statusButton.innerHTML = "Not Read";
+                statusButton.style.backgroundColor = "red";
+                div.style.borderLeft = "10px solid red";
+            }
+            else {
+                library[parseInt(statusToChange)].status = "Read";
+                statusButton.innerHTML = "Read";
+                statusButton.style.backgroundColor = "green";
+                div.style.borderLeft = "10px solid green";
+            }
+        }
+
+        const rmvButton = document.createElement('button');
+        rmvButton.classList.add("remove");
+        rmvButton.dataset.id = `${i}`;
+        div.appendChild(rmvButton);
+
+        rmvButton.addEventListener("click", removeBook);
+        function removeBook(){
+            let bookToRemove = rmvButton.dataset.id;
+            if (confirm(`Are you sure to delete ${title.innerHTML} ?`)) {
+                library.splice(parseInt(bookToRemove), 1);
+                renderBooks();
+            }
+        }
+
+        title.innerHTML = `Book Name: ${library[i].name}`;
+        para.innerHTML = `Author Name: ${library[i].author}`;
+        statusButton.innerHTML = library[i].status;
+        rmvButton.innerHTML = "Remove"
     }
 }
-renderBooks()
